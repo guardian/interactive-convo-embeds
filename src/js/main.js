182f217e0d1ac86bf1eeceaf86965a80d030c3f4
Matt Osborn <matthew.osborn@theguardian.com>
2015-11-26 10:38:11 +0000

    between styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,83 +1,91 @@
 import doT from 'olado/doT'
-import mainTemplate from '../templates/main.html!text'
+import thatTemplate from '../templates/that.html!text'
+import betweenTemplate from '../templates/between.html!text'
 import bean from 'fat/bean'
 import bowser from 'ded/bowser'
 import bonzo from 'ded/bonzo'
 import reqwest from 'reqwest'
 import {fetchJSON} from './lib/fetch'
 
-var renderMainTemplate = doT.template(mainTemplate);
-let jsonURL = 'https://interactive.guim.co.uk/docsdata/1O84T2QVZ5CUWELQ4_ZfdfctxuTTP2JE0xropElb7DAo.json'
+let jsonURL = 'https://interactive.guim.co.uk/docsdata-test/1O84T2QVZ5CUWELQ4_ZfdfctxuTTP2JE0xropElb7DAo.json'
+
+export function between(el, context) {
+    init(el, context, 'article', doT.template(betweenTemplate))
+}
 
 export function that(el, context) {
-	fetchJSON(jsonURL).then(spreadsheet => {
-    	var contents = spreadsheet.sheets.interactive;
-		el.innerHTML = renderMainTemplate({
-			contents: contents
-		})
+    init(el, context, 'interactive', doT.template(thatTemplate))
+}
 
-		var embedEl = el.querySelector('.embed');
+export function init(el, context, contentKey, templateFn) {
+    fetchJSON(jsonURL).then(spreadsheet => {
+        var contents = spreadsheet.sheets[contentKey];
+        el.innerHTML = templateFn({
+            contents: contents
+        })
 
-		var firstTitleEl = el.querySelector('.title');
-		var currentIndex = 0;
+        var embedEl = el.querySelector('.embed');
 
-		function goToIndex(index) {
-			currentIndex = index;
-			firstTitleEl.style.marginLeft = `${currentIndex * -100}%`;
-			embedEl.setAttribute('meta', currentIndex === 0 ? 'first' : currentIndex === contents.length-1 ? 'last' : '');
-			embedEl.href = contents[currentIndex].url;
-		}
+        var firstTitleEl = el.querySelector('.title');
+        var currentIndex = 0;
 
-		var goToNext = () => {
-			var nextIndex = currentIndex === contents.length-1 ? 0 : currentIndex + 1;
-			goToIndex(nextIndex);
-		};
+        function goToIndex(index) {
+            currentIndex = index;
+            firstTitleEl.style.marginLeft = `${currentIndex * -100}%`;
+            embedEl.setAttribute('meta', currentIndex === 0 ? 'first' : currentIndex === contents.length-1 ? 'last' : '');
+            embedEl.href = contents[currentIndex].url;
+        }
 
-		var goToPrev = () => {
-			var nextIndex = currentIndex === 0 ? contents.length-1 : currentIndex - 1;
-			goToIndex(nextIndex);
-		};
+        var goToNext = () => {
+            var nextIndex = currentIndex === contents.length-1 ? 0 : currentIndex + 1;
+            goToIndex(nextIndex);
+        };
 
-		embedEl.addEventListener('mouseenter', evt => {
-			bonzo(embedEl).addClass('embed--hover');
-			stopCarousel();
-		})
+        var goToPrev = () => {
+            var nextIndex = currentIndex === 0 ? contents.length-1 : currentIndex - 1;
+            goToIndex(nextIndex);
+        };
 
-		embedEl.addEventListener('mouseleave', evt => {
-			bonzo(embedEl).removeClass('embed--hover');
-			startCarousel();
-		})
+        embedEl.addEventListener('mouseenter', evt => {
+            bonzo(embedEl).addClass('embed--hover');
+            stopCarousel();
+        })
 
-		var $embedEl = bonzo(embedEl),
-			prevEl = embedEl.querySelector('.prev'),
-			nextEl = embedEl.querySelector('.next');
+        embedEl.addEventListener('mouseleave', evt => {
+            bonzo(embedEl).removeClass('embed--hover');
+            startCarousel();
+        })
 
-		prevEl.addEventListener('mouseenter', evt => $embedEl.addClass('embed--fast-transition'))
-		prevEl.addEventListener('mouseleave', evt => $embedEl.removeClass('embed--fast-transition'))
-		nextEl.addEventListener('mouseenter', evt => $embedEl.addClass('embed--fast-transition'))
-		nextEl.addEventListener('mouseleave', evt => $embedEl.removeClass('embed--fast-transition'))
+        var $embedEl = bonzo(embedEl),
+            prevEl = embedEl.querySelector('.prev'),
+            nextEl = embedEl.querySelector('.next');
 
-		prevEl.addEventListener('click', evt => {
-			evt.preventDefault();
-			if (currentIndex !== 0) {
-				goToPrev();
-				bonzo(embedEl).addClass('embed--fast-transition')
-			}
-		})
+        prevEl.addEventListener('mouseenter', evt => $embedEl.addClass('embed--fast-transition'))
+        prevEl.addEventListener('mouseleave', evt => $embedEl.removeClass('embed--fast-transition'))
+        nextEl.addEventListener('mouseenter', evt => $embedEl.addClass('embed--fast-transition'))
+        nextEl.addEventListener('mouseleave', evt => $embedEl.removeClass('embed--fast-transition'))
+
+        prevEl.addEventListener('click', evt => {
+            evt.preventDefault();
+            if (currentIndex !== 0) {
+                goToPrev();
+                bonzo(embedEl).addClass('embed--fast-transition')
+            }
+        })
 
 
-		nextEl.addEventListener('click', evt => {
-			evt.preventDefault();
-			if (currentIndex !== contents.length-1) {
-				goToNext();
-			}
-		})
+        nextEl.addEventListener('click', evt => {
+            evt.preventDefault();
+            if (currentIndex !== contents.length-1) {
+                goToNext();
+            }
+        })
 
-		var interval,
-			startCarousel = () => interval = window.setInterval(goToNext, 5000),
-			stopCarousel = () => window.clearInterval(interval);
+        var interval,
+            startCarousel = () => interval = window.setInterval(goToNext, 5000),
+            stopCarousel = () => window.clearInterval(interval);
 
-		startCarousel();
-		goToIndex(0);
+        startCarousel();
+        goToIndex(0);
     })
 }
